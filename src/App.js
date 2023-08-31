81b62dcd1c1baaa4c9587fef26d6aad13c684393
Mickey <playmickey0601@gmail.com>
2023-08-31 12:16:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import TagView from './TagView'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialTree = {
+  name: 'root',
+  children: [
+    {
+      name: 'child1',
+      children: [
+        { name: 'child1-child1', data: 'c1-c1 Hello' },
+        { name: 'child1-child2', data: 'c1-c2 JS' },
+      ],
+    },
+    { name: 'child2', data: 'c2 World' },
+  ],
 }
 
-export default App;
+function App() {
+  const [tree, setTree] = useState(initialTree)
+  const [exportedJSON, setExportedJSON] = useState('')
+
+  const recursiveFindTag = (tree, name) => {
+    if (tree.hasOwnProperty('name') && tree.name === name) {
+      return tree
+    }
+    if (tree.hasOwnProperty('children')) {
+      for (const child of tree.children) {
+        const foundTag = recursiveFindTag(child, name)
+        if (foundTag != null) {
+          return foundTag
+        }
+      }
+    }
+    return null
+  }
+
+  const recursiveUpdateData = (tree, name, data) => {
+    const tag = recursiveFindTag(tree, name)
+    if (tag != null) {
+      tag.data = data
+      return true
+    }
+  }
+
+  const recursiveAddChild = (tree, name) => {
+    const tag = recursiveFindTag(tree, name)
+    const children = tag.children ? tag.children : null
+    const numChildren = children != null ? children.length : 0
+    const dummyData =
+      `${name}-child${numChildren + 1}`.replaceAll('child', 'c') + ' Hello'
+    if (tag != null) {
+      if (children == null) {
+        tag.children = []
+      }
+      tag.children.push({
+        name: `${name}-child${numChildren + 1}`,
+        data: dummyData,
+      })
+      return true
+    }
+  }
+
+  const recursiveUpdateTagName = (tag, oldName, newName) => {
+    if (tag.name === oldName) {
+      tag.name = newName
+    }
+    if (tag.children) {
+      tag.children.forEach((child) => {
+        recursiveUpdateTagName(child, oldName, newName)
+      })
+    }
+  }
+
+  const updateTag = (tag, newData) => {
+    const updatedTree = JSON.parse(JSON.stringify(tree))
+    recursiveUpdateData(updatedTree, tag.name, newData)
+    setTree(updatedTree)
+  }
+
+  const addChildTag = (parentTagName) => {
+    const updatedTree = JSON.parse(JSON.stringify(tree))
+    recursiveAddChild(updatedTree, parentTagName)
+    setTree(updatedTree)
+  }
+
+  const updateTagName = (tag, newName) => {
+    const updatedTree = JSON.parse(JSON.stringify(tree))
+    recursiveUpdateTagName(updatedTree, tag.name, newName)
+    setTree(updatedTree)
+  }
+
+  return (
+    <div className="App">
+      <TagView
+        tag={tree}
+        onUpdate={updateTag}
+        onAddChild={addChildTag}
+        onUpdateTagName={updateTagName}
+      />
+      <button onClick={() => setExportedJSON(JSON.stringify(tree, null, 2))}>
+        Export
+      </button>
+      <pre>{exportedJSON}</pre>
+    </div>
+  )
+}
+
+export default App
---
+++ src/TagView.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+
+const TagView = ({ tag, onUpdate, onAddChild, onUpdateTagName }) => {
+  const [collapsed, setCollapsed] = useState(false)
+  const [editingName, setEditingName] = useState(false)
+  const [newName, setNewName] = useState(tag.name)
+
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed)
+  }
+
+  const handleNameEdit = () => {
+    setEditingName(true)
+  }
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const handleNameSubmit = (e) => {
+    e.preventDefault()
+    onUpdateTagName(tag, newName)
+    setEditingName(false)
+  }
+
+  const renderChildren = () => {
+    if (collapsed) {
+      return null
+    }
+    if (tag.children) {
+      return (
+        <div className="children">
+          {tag.children.map((child) => (
+            <TagView
+              key={child.name}
+              tag={child}
+              onUpdate={onUpdate}
+              onAddChild={onAddChild}
+              onUpdateTagName={onUpdateTagName}
+            />
+          ))}
+        </div>
+      )
+    }
+    return (
+      <div className="data">
+        <div className="text">Data</div>
+        <input
+          type="text"
+          value={tag.data}
+          onChange={(e) => onUpdate(tag, e.target.value)}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div className="tag">
+      <div className="tag-header">
+        <span className="collapse" onClick={toggleCollapse}>
+          {collapsed ? '>' : 'v'}
+        </span>
+        {editingName ? (
+          <form onSubmit={handleNameSubmit}>
+            <input
+              type="text"
+              value={newName}
+              onChange={handleNameChange}
+              onBlur={handleNameSubmit}
+              autoFocus
+            />
+          </form>
+        ) : (
+          <span className="tag-name" onClick={handleNameEdit}>
+            {newName}
+          </span>
+        )}
+        <button onClick={() => onAddChild(tag.name)}>Add Child</button>
+      </div>
+      {renderChildren()}
+    </div>
+  )
+}
+
+export default TagView
